1eca4c5ee8f0efd50aeedb73efd901e27caf1326
Cici Woolf <christinemarie.woolf@gmail.com>
2024-08-28 21:40:44 -0500

    initial setup

---
+++ fun-stuff/app/about/page.js
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h2>My name is Christine Woolf, but please, call me Cici for short. </h2>
+    </main>
+  );
+}
---
+++ fun-stuff/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>🔧 Project in Construction 🔧</h1>
+    </main>
+  );
+}
