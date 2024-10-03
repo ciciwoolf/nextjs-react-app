47a43a450c57d5986efb73ed0ae90b00e122b054
Cici Woolf <christinemarie.woolf@gmail.com>
2024-10-03 06:58:36 -0500

    Add some animated whirly thing

--- fun-stuff/app/page.js
+++ fun-stuff/app/page.js
@@ -1,17 +1,16 @@
 import "./globals.scss";
 import styles from "./page.module.scss";
 
-const homeContent = "Welcome to my Website";
-const greeting = "Hello";
+const homeContent = "Welcome to my website";
 
 export default function Home() {
   return (
     <div>
       <header className={styles.header}></header>
       <div className={styles.hero}>
-        <p className={styles.animate}>{greeting}</p>
-        <p>{homeContent}</p>
+        <p className={styles.greeting}>{homeContent}</p>
       </div>
+      <div className={styles.colorsBody}></div>
     </div>
   );
 }
