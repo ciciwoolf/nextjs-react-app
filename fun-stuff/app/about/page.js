98f2af2db6d1dd5f06d3b47b9a39f6ea3707180f
Cici Woolf <christinemarie.woolf@gmail.com>
2024-10-03 10:27:38 -0500

    Update content a bit

--- fun-stuff/app/about/page.js
+++ fun-stuff/app/about/page.js
@@ -3,32 +3,38 @@ import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import styles from "./about-page.module.scss";
 
-const aboutContent1 = "My name is Christine Woolf but I go by Cici";
-const aboutContent2 = "I go by Cici or C^2 :-)";
-const aboutContent3 = "I'm a Fullstack Software Developer";
+// Array of messages
+const aboutMessages = [
+  "My name is Christine Woolf",
+  "I go by Cici or C^2 :-)",
+  "I'm a Fullstack Software Developer",
+];
 
 export default function AboutPage() {
-  const [currentMsg, setCurrentMsg] = useState(aboutContent1);
-
+  const [currentMsgIndex, setCurrentMsgIndex] = useState(0); // Track the index of the current message
   const [animation, setAnimation] = useState(styles.slideIn);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setAnimation(styles.slideOut);
+
       setTimeout(() => {
-        setCurrentMsg((prev) =>
-          prev === aboutContent1 ? aboutContent2 : aboutContent1
+        setCurrentMsgIndex((prevIndex) =>
+          prevIndex === aboutMessages.length - 1 ? 0 : prevIndex + 1
         );
+
         setAnimation(styles.slideIn);
       }, 500);
-    }, 3500); // change msg every 3 sec
+    }, 3500);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <main styles={styles.aboutMain}>
-      <p className={`${styles.aboutText} ${animation}`}>{currentMsg}</p>
+    <main className={styles.aboutMain}>
+      <p className={`${styles.aboutText} ${animation}`}>
+        {aboutMessages[currentMsgIndex]}
+      </p>
     </main>
   );
 }
