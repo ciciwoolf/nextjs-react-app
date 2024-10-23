56f5f7baa4a255f2aab8db000556217c0e4decd4
Cici Woolf <christinemarie.woolf@gmail.com>
2024-10-22 21:39:00 -0500

    Adding videos

--- fun-stuff/app/projects/art/page.js
+++ fun-stuff/app/projects/art/page.js
@@ -1,17 +1,43 @@
+"use client";
 import Link from "next/link";
 
-export default function ProjectPage({ params }) {
+import ReactPlayer from "react-player/lazy";
+import styles from "./page.module.scss";
+
+export default function ProjectPage() {
   return (
-    <main>
+    <main class={styles.artPage}>
       <h1>Art</h1>
-      <p>
-        <Link href="https://github.com/ciciwoolf/javascript-art">
-          Check out my repo for my JavaScript art
-        </Link>
-      </p>
-      <p>
-        <Link href="/projects">Back to Projects</Link>
-      </p>
+      <div class={styles.videoContainer}>
+        <div class={styles.video}>
+          <ReactPlayer
+            url="https://vimeo.com/1022338992"
+            onReady
+            playing="true"
+            loop="true"
+          />
+          <p>SpiderWeb</p>
+        </div>
+        <div class={styles.video}>
+          <ReactPlayer
+            url="https://vimeo.com/1022340807"
+            onReady
+            playing="true"
+            loop="true"
+          />
+          <p>Violet River</p>
+        </div>
+      </div>
+      <div class={styles.links}>
+        <p>
+          <Link href="https://github.com/ciciwoolf/javascript-art">
+            Click here to see more projects on Github
+          </Link>
+        </p>
+        <p>
+          <Link href="/projects">Back to Projects</Link>
+        </p>
+      </div>
     </main>
   );
 }
--- fun-stuff/app/projects/bootcamp/page.js
+++ fun-stuff/app/projects/bootcamp/page.js
@@ -4,6 +4,12 @@ export default function ProjectPage({ params }) {
   return (
     <main>
       <h1> Bootcamp Project </h1>
+      <iframe
+        width="800"
+        height="450"
+        src="https://embed.figma.com/design/aLlIsyRhgVZjoAcOcs8K86/Cici's-Chocolates?node-id=0-1&embed-host=share"
+        allowfullscreen
+      ></iframe>
       <p>
         <Link href="https://github.com/ciciwoolf/proyecto_ciciwoolf_digitalhouse">
           Check out my Digitalhouse Bootcamp Project repo
